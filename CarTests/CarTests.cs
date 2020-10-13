78fd58baf5aca39d57dba285fa5a29a96979d1aa
Randy Parker <randallbparker@gmail.com>
2020-10-13 15:00:22 -0500

    TDD Exercises Passing Tests

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,14 +1,62 @@
+using CarNS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarTests
 {
+    [TestClass]
     public class CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
-        //TODO: constructor sets gasTankLevel properly
-        //TODO: gasTankLevel is accurate after driving within tank range
-        //TODO: gasTankLevel is accurate after attempting to drive past tank range
-        //TODO: can't have more gas than tank size, expect an exception
+        //[TestMethod]
+        //public void EmptyTest()
+        //{
+        //    Assert.AreEqual(10, 10, .001);
+        //}
 
+        Car test_car;
+
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+
+        }
+
+        //TODO: constructor sets gasTankLevel properly
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
+        //TODO: gasTankLevel is accurate after driving within tank range
+        [TestMethod]
+        public void TestGasTankAfterDriving()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+
+        //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(550);
+            Assert.AreEqual(0, test_car.GasTankLevel, .001);
+        }
+        //TODO: can't have more gas than tank size, expect an exception
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+        }
+
+        //[TestInitialize] .. done before each test
+
+        //AreEqual(expected, actual, optional_delta)  Asserts that two values, expected and actual, are equal to each other(optionally, within a given range of difference)
+        //IsFalse(condition)  Asserts that a given condition is false
+        //IsTrue(condition)   Asserts that a given condition is true
+        //IsNotNull(object)   Asserts that a given object is not null
     }
 }
